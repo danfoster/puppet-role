4b9f8e651974441dc414bc7e8e85b9df6606a2d9
Dan Foster <dan.foster@bristol.ac.uk>
2015-02-06 12:13:08 +0000

    Tidy up

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,7 @@
-# == Class: roles
+# == Class: role
 #
-# Full description of class roles here.
-#
-# === Examples
-#
-#  class { 'roles':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
-class roles (
-) inherits ::roles::params {
+class role (
+) inherits ::role::params {
 
 
 }
