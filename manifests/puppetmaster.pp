84f04fd60e0747b0850f716b8f605d8dd5beb246
Dan Foster <dan.foster@bristol.ac.uk>
2015-02-06 11:44:30 +0000

    Add foreman to puppetmaster role

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,5 +1,5 @@
 # A Role for the puppetmaster
-class roles::puppetmaster {
+class role::puppetmaster {
   selinux::module { 'puppet':
     ensure => 'present',
     source => 'puppet:///modules/roles/puppetmaster/puppet.te',
@@ -27,5 +27,21 @@ class roles::puppetmaster {
     ensure => file,
     source => 'puppet:///modules/roles/puppetmaster/hiera.yaml',
   }
+
+  include foreman
+  include foreman_proxy
+
+
+  class { 'puppetdb': }
+  class { 'puppetdb::master::config':
+    manage_storeconfigs => false
+  }
+    
+  postgresql::server::db { $foreman::db_database:
+    user     => $foreman::db_username,
+    password => $foreman::db_password,
+    owner    => $foreman::db_username,
+  }
+
 
 }
