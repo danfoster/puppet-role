402d4f475ea7329fddf483a70e8aa1acdb7ae8a0
Dan Foster <dan@zem.org.uk>
2015-03-10 16:35:42 +0000

    Fix typo

--- manifests/samba.pp
+++ manifests/samba.pp
@@ -3,7 +3,7 @@ class role::samba {
   class { '::samba::server':
     map_to_guest             => 'Bad User',
     preferred_master         => 'yes',
-    shares                   => hiera_hash($samba_shares,{}),
+    shares                   => hiera_hash('samba_shares',{}),
     selinux_enable_home_dirs => true,
     extra_global_options     => [
       'usershare path = /var/lib/samba/usershares',
